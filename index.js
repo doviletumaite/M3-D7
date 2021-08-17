4759660f58ed86b776d891c27398e492b6eb92a8
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-08-17 16:34:46 +0200

    3

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+let userArr = []
 const row = document.getElementById("row");
+const input = document.getElementById("input")
 const getUser = async () => {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const user = await response.json();
     console.log(user);
-
+   userArr = user
     displayCard(user);
   } catch (err) {
     console.log(err);
@@ -13,6 +15,7 @@ const getUser = async () => {
 
 const displayCard = (usersArr) => {
   usersArr.forEach((person) => {
+    
     let userAddress = getAddress(person);
     row.innerHTML += `
         <div class="col-sm-6 col-md-4 mt-3">
@@ -41,6 +44,41 @@ const getAddress = (userObj) => {
 
   return address.join(" ");
 };
+
+
+
+const searchUser = (event) => {
+  // console.log(event)
+  if (event.key === "Enter" && event.target.value.length > 3) {
+    const filteredUsers = userArr.filter(user => {
+      if (user.name.toLowerCase().includes(event.target.value.toLowerCase())){
+        return user
+      }
+    })
+    console.log(filteredUsers)
+    displayFilteredCardUser(filteredUsers)
+  }
+ }
+
+ const displayFilteredCardUser = (person) => {
+    row.innerHTML = ""
+    let userAddress = getAddress(person);
+    row.innerHTML += `
+        <div class="col-sm-6 col-md-4 mt-3">
+            <div class="card mr-3" ">
+                <div class="card-body">
+                <h5 class="card-title">${person.name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Username: ${person.username}</h6>
+                <p class="card-text">${userAddress}</p>
+                <a href="#" class="card-link">Email: ${person.email}</a>
+                <p class="text-muted" style="font-size: 10px">id: ${person.id}</p>
+                </div>
+          </div>
+       </div>
+        `;
+  
+}
+
 
 window.onload = () => {
   getUser();
