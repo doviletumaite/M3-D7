2dd3c66eedfcd8cb7d391d7add713fd93ff3e215
Azizbek Tokhirjonov <azizwebdev77@gmail.com>
2021-08-17 16:48:03 +0200

    fixing search functionality

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let userArr = []
+let userArr = [];
 const row = document.getElementById("row");
-const input = document.getElementById("input")
+const input = document.getElementById("input");
 const getUser = async () => {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const user = await response.json();
     console.log(user);
-   userArr = user
+    userArr = user;
     displayCard(user);
   } catch (err) {
     console.log(err);
@@ -15,8 +15,7 @@ const getUser = async () => {
 
 const displayCard = (usersArr) => {
   usersArr.forEach((person) => {
-    
-    let userAddress = getAddress(person);
+    let userAddress = getAddress2(person);
     row.innerHTML += `
         <div class="col-sm-6 col-md-4 mt-3">
             <div class="card mr-3" ">
@@ -33,7 +32,21 @@ const displayCard = (usersArr) => {
   });
 };
 
-const getAddress = (userObj) => {
+const getAddress = (userAddress) => {
+  const address = [];
+  userAddress.forEach((userObj) => {
+    address.push(
+      userObj.address.street,
+      userObj.address.suite,
+      userObj.address.city,
+      userObj.address.zipcode
+    );
+  });
+
+  return address.join(" ");
+};
+
+const getAddress2 = (userObj) => {
   const address = [];
   address.push(
     userObj.address.street,
@@ -45,24 +58,10 @@ const getAddress = (userObj) => {
   return address.join(" ");
 };
 
-
-
-const searchUser = (event) => {
-  // console.log(event)
-  if (event.key === "Enter" && event.target.value.length > 3) {
-    const filteredUsers = userArr.filter(user => {
-      if (user.name.toLowerCase().includes(event.target.value.toLowerCase())){
-        return user
-      }
-    })
-    console.log(filteredUsers)
-    displayFilteredCardUser(filteredUsers)
-  }
- }
-
- const displayFilteredCardUser = (person) => {
-    row.innerHTML = ""
-    let userAddress = getAddress(person);
+const displayFilteredCardUser = (usersArray) => {
+  row.innerHTML = "";
+  let userAddress = getAddress(usersArray);
+  usersArray.forEach((person) => {
     row.innerHTML += `
         <div class="col-sm-6 col-md-4 mt-3">
             <div class="card mr-3" ">
@@ -76,9 +75,21 @@ const searchUser = (event) => {
           </div>
        </div>
         `;
-  
-}
+  });
+};
 
+const searchUser = (event) => {
+  // console.log(event)
+  if (event.key === "Enter" && event.target.value.length > 3) {
+    const filteredUsers = userArr.filter((user) => {
+      if (user.name.toLowerCase().includes(event.target.value.toLowerCase())) {
+        return user;
+      }
+    });
+    console.log(filteredUsers);
+    displayFilteredCardUser(filteredUsers);
+  }
+};
 
 window.onload = () => {
   getUser();
